b292a4e8399b9ebf483c1280c677bba65e02c1c8
Maxim Tuleyko <mtuleyko@gmail.com>
2022-08-30 15:25:33 +0200

    Fix Hardhat config #2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,17 +4,19 @@ import '@openzeppelin/hardhat-upgrades';
 import "dotenv/config";
 import {ZERO_PRIVKEY} from "./scripts/utils/constants";
 
+const HARDHAT_NETWORK = 'http://127.0.0.1:8545/';
+
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 5,
       forking: {
-        url: process.env.INFURA_GOERLI_RPC || 'oops'
+        url: process.env.INFURA_GOERLI_RPC || HARDHAT_NETWORK
       }
     },
     goerli: {
-      url: process.env.INFURA_GOERLI_RPC || 'oops',
+      url: process.env.INFURA_GOERLI_RPC || HARDHAT_NETWORK,
       accounts: [process.env.GOERLI_DEPLOYER_PRIVKEY || ZERO_PRIVKEY]
     }
   },
