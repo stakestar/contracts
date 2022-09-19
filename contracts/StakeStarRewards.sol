89ef09eab28d1d401cae701ee3c77f2e32cf1d46
Maxim Tuleyko <mtuleyko@gmail.com>
2022-09-19 12:02:26 +0200

    Replace DEFAULT_ADMIN_ROLE with STAKE_STAR_ROLE

--- contracts/StakeStarRewards.sol
+++ contracts/StakeStarRewards.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract StakeStarRewards is AccessControl {
+    bytes32 public constant STAKE_STAR_ROLE = keccak256("StakeStar");
+
     constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(STAKE_STAR_ROLE, msg.sender);
     }
 
     receive() external payable {}
 
-    function pull() public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function pull() public onlyRole(STAKE_STAR_ROLE) {
         payable(msg.sender).transfer(address(this).balance);
     }
 }
