e84b8bb268a3741740919235485976984b140b4d
Stas Arsoba <s@onestar.dev>
2022-10-25 16:25:51 +0200

    Remove Access Control from MockRewardsProvider

--- test/mockRewardsProvider.ts
+++ test/mockRewardsProvider.ts
@@ -1,45 +1,8 @@
 import { expect } from "chai";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { deployStakeStarFixture } from "./fixture";
-import { Wallet } from "ethers";
-import { ZERO_BYTES_STRING } from "../scripts/constants";
-import { ValidatorStatus } from "../scripts/types";
 
 describe("MockRewardsProvider", function () {
-  describe("Deployment", function () {
-    it("Should set the right roles", async function () {
-      const { mockRewardsProvider, owner, otherAccount } =
-        await loadFixture(deployStakeStarFixture);
-
-      expect(
-        await mockRewardsProvider.hasRole(
-          await mockRewardsProvider.DEFAULT_ADMIN_ROLE(),
-          owner.address
-        )
-      ).to.equal(true);
-      expect(
-        await mockRewardsProvider.hasRole(
-          await mockRewardsProvider.DEFAULT_ADMIN_ROLE(),
-          otherAccount.address
-        )
-      ).to.equal(false);
-    });
-  });
-
-  describe("AccessControl", function () {
-    it("Should not allow call methods without admin role", async function () {
-      const { mockRewardsProvider, otherAccount } = await loadFixture(
-        deployStakeStarFixture
-      );
-
-      await expect(
-        mockRewardsProvider.connect(otherAccount).provideRewards(123)
-      ).to.be.revertedWith(
-        `AccessControl: account ${otherAccount.address.toLowerCase()} is missing role ${await mockRewardsProvider.DEFAULT_ADMIN_ROLE()}`
-      );
-    });
-  });
-
   describe("Provide rewards", function () {
     it("Should store updated rewards", async function () {
       const { mockRewardsProvider, owner } = await loadFixture(
