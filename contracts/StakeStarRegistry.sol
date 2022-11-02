9ace1b5b1ca832c22b8ab1f649f9e3a1e634d2a8
Maxim Tuleyko <mtuleyko@gmail.com>
2022-11-02 16:03:45 +0300

    StakeStarRegistry implements PoRAddressList interface

--- contracts/StakeStarRegistry.sol
+++ contracts/StakeStarRegistry.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {PoRAddressList} from "@chainlink/contracts/src/v0.8/interfaces/PoRAddressList.sol";
 
-contract StakeStarRegistry is Initializable, AccessControlUpgradeable {
+contract StakeStarRegistry is Initializable, AccessControlUpgradeable, PoRAddressList {
     event AddOperatorToAllowList(uint32 operatorId);
     event RemoveOperatorFromAllowList(uint32 operatorId);
     event CreateValidator(bytes publicKey);
@@ -51,10 +52,14 @@ contract StakeStarRegistry is Initializable, AccessControlUpgradeable {
         emit DestroyValidator(publicKey);
     }
 
+    function getValidatorPublicKeysLength() public view returns (uint256) {
+        return validatorPublicKeys.length;
+    }
+
     function getValidatorPublicKeys(ValidatorStatus status) public view returns (bytes[] memory publicKeys) {
         publicKeys = new bytes[](validatorPublicKeys.length);
 
-        for (uint32 i = 0; i < validatorPublicKeys.length; i++) {
+        for (uint256 i = 0; i < validatorPublicKeys.length; i++) {
             bytes memory publicKey = validatorPublicKeys[i];
             if (validatorStatuses[publicKey] == status) {
                 publicKeys[i] = publicKey;
@@ -62,18 +67,58 @@ contract StakeStarRegistry is Initializable, AccessControlUpgradeable {
         }
     }
 
-    function countValidatorPublicKeys(ValidatorStatus status) public view returns (uint32 count) {
+    function countValidatorPublicKeys(ValidatorStatus status) public view returns (uint256 count) {
         count = 0;
 
-        for (uint32 i = 0; i < validatorPublicKeys.length; i++) {
-            bytes memory publicKey = validatorPublicKeys[i];
-            ValidatorStatus validatorStatus = validatorStatuses[publicKey];
-            if (status == ValidatorStatus.CREATED &&
-                (validatorStatus == ValidatorStatus.CREATED || validatorStatus == ValidatorStatus.DESTROYED)) {
-                count++;
-            } else if (status == ValidatorStatus.DESTROYED && validatorStatus == ValidatorStatus.DESTROYED) {
+        for (uint256 i = 0; i < validatorPublicKeys.length; i++) {
+            if (status == validatorStatuses[validatorPublicKeys[i]]) {
                 count++;
             }
         }
+    }
+
+    function getPoRAddressListLength() public view returns (uint256) {
+        return countValidatorPublicKeys(ValidatorStatus.CREATED);
+    }
+
+    function getPoRAddressList(uint256 startIndex, uint256 endIndex) public view returns (string[] memory) {
+        uint256 length = getPoRAddressListLength();
+        if (length == 0) {
+            return new string[](0);
+        }
+
+        uint256 normalizedEndIndex = endIndex < length - 1 ? endIndex : length - 1;
+        require(startIndex <= normalizedEndIndex, "startIndex must be less than or equal to normalizedEndIndex");
+
+        string[] memory addressList = new string[](normalizedEndIndex - startIndex + 1);
+        bytes[] memory publicKeys = getValidatorPublicKeys(ValidatorStatus.CREATED);
+        uint256 relativeIndex = 0;
+        uint256 returnIndex = 0;
+
+        for (uint256 i = 0; i < publicKeys.length; i++) {
+            bytes memory publicKey = publicKeys[i];
+            if (publicKey.length > 0) {
+                if (relativeIndex >= startIndex && relativeIndex <= endIndex) {
+                    addressList[returnIndex] = toString(publicKey);
+                    returnIndex++;
+                }
+                relativeIndex++;
+            }
+        }
+
+        return addressList;
+    }
+
+    function toString(bytes memory data) private pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(2 + data.length * 2);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint256 i = 0; i < data.length; i++) {
+            str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
+            str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
+        }
+        return string(str);
     }
 }
