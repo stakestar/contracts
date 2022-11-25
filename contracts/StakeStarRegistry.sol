6a08e52b91f7acdcb50d906a5b36e20b6e008c8a
Maxim Tuleyko <mtuleyko@gmail.com>
2022-11-25 16:04:11 +0300

    commit solidity 0.8.17 for stakestar contracts

--- contracts/StakeStarRegistry.sol
+++ contracts/StakeStarRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
