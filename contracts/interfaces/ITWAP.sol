e7ed1e93dcb64c578409ff397873b6a4399c1a6a
Maxim Tuleyko <mtuleyko@gmail.com>
2023-02-13 21:11:42 +0300

    implement TWAP/ITWAP

---
+++ contracts/interfaces/ITWAP.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+interface ITWAP {
+    function getSqrtTwapX96(
+        address uniswapV3Pool,
+        uint32 twapInterval
+    ) external view returns (uint160 sqrtPriceX96);
+
+    function getPriceX96FromSqrtPriceX96(
+        uint160 sqrtPriceX96
+    ) external pure returns (uint256 priceX96);
+}
