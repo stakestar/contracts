b3173560c95a38ffbbfa472d88dffdaaeb53cbac
Maxim Tuleyko <mtuleyko@gmail.com>
2023-02-14 16:15:10 +0300

    remove hardhat/console

--- contracts/StakeStarTreasury.sol
+++ contracts/StakeStarTreasury.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
